a321ae2d9d04467141e1086c07c0674c710d5fba
Your Name <you@example.com>
2023-07-18 14:30:46 -0400

    cases dont work in main.c

--- redirection.c
+++ redirection.c
@@ -7,7 +7,7 @@ void	here_doc(char *end)
 	int		fd;
 
 	s = ft_strjoin(end, "\n");
-	fd = open("tmp", O_RDWR | O_CREAT | O_TRUNC, 0644);
+	fd = open("tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd < 0)
 		return ;
 	while (1)
@@ -19,11 +19,8 @@ void	here_doc(char *end)
 			free(line);
 			break ;
 		}
-		else
-		{
-			write(fd, line, ft_strlen(line));
-			free(line);
-		}
+		write(fd, line, ft_strlen(line));
+		free(line);
 	}
 	close(fd);
 	free(s);
@@ -36,6 +33,7 @@ int	redirection(t_vars *vars, t_cmds **cmds, int i)
 			return (0);
 	else if ((*cmds)[i].in_stat == 2)
 	{
+		
 		here_doc((*cmds)[i].red_in);
 		stop_program(dup2(open("tmp", O_RDONLY), 0) == -1, "", IO, vars->exit_stat);
 	}
