d3827c4dc8f114647a46a1ca5ac984f4c4b9cfaf
Lyudmila <lyudakstnn@gmail.com>
2023-10-01 01:26:21 +0400

    here_doc

--- src/rm_quotes.c
+++ src/rm_quotes.c
@@ -19,10 +19,13 @@ void	rm_q_if(t_vars *vars, char **tmp, int *i, char main_c)
 	if (**tmp == '<' && *(*tmp + 1) && *(*tmp + 1) == **tmp)
 	{
 		vars->here_doc++;
-		if (*(*tmp + 2) == 32)
-			j = 3;
-		else
-			j = 2;
+		// if (*(*tmp + 2) == 32)
+		// 	j = 3;
+		// else
+		// 	j = 2;
+		j = 1;
+		while ((*tmp)[++j] && (*tmp)[j] == 32)
+			;
 		vars->hd_stat = 0;
 		while ((*tmp)[j] && (*tmp)[j] != 32 && (*tmp)[j] != main_c)
 			j++;
